49c4740325c0a887853d2d36b4e5f931ba787964
khsexk <kohyunsuk98@gmail.com>
2022-03-12 23:54:47 +0900

    2-4까지

---
+++ 02_Practice/Ch02/practice2-5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(void) {
+    char password[11];
+
+    cout << "프로그램을 종료하려면 암호를 입력하세요." << endl;
+
+    while(true){
+        cout << "암호>> ";
+        cin >>password;
+
+        if(strcmp(password, "C++") == 0){
+            cout << "프로그램 종료" << endl;
+            break;
+        }
+        else
+            cout << "암호가 틀립니다" << endl;
+    }
+}
